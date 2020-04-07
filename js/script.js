a77812e0d0c5b95dd76db9ea93ff1b23fa1005e5
Alan <alann0918@gmail.com>
2020-04-06 21:56:31 -0400

    changed the whole websitye

--- js/script.js
+++ js/script.js
@@ -9,11 +9,12 @@ $(document).ready(function() {
         var q2Result = $("#question2").val();
         var q3Result = $("#question3").val();
         var totalScore;
-        totalScore = CalculateQuestion1Score(q1Result);
+        totalScore = totalScore + CalculateQuestion1Score(q1Result);
         totalScore = totalScore + CalculateQuestion2Score(q2Result);
         totalScore = totalScore + CalculateQuestion3Score(q3Result);
         //console.log(totalScore);
         placement(totalScore,name);
+        console.log(totalScore)
     });
 
 
@@ -23,50 +24,43 @@ $(document).ready(function() {
     }
 
     function CalculateQuestion1Score(question1) {
-        if (question1 === " a pillowmet" || question1 === "pillowmet") {
+        if (question1 === " keep it" || question1 === "Keep it") {
             return 2;
         }
-        else if (question1 === "a tissue" || question1 === "tissue") {
+        else if (question1 === "Police" || question1 === "police") {
             return 3;
         }
-        else if (question1 === "a stinky sock" ||  question1 === "stinky sock" || question1 === "sock") {
-            return 4;
-        }
-        else if (question1 === " a durag" || question1 === "durag") {
-            return 5;
-        }
+
         else {
-            return 0;
+            return 1;
         }
     }
 
     function CalculateQuestion2Score(question2) {
-        if (question2 === "takeoff" || question2 === "Takeoff") {
-            return 1;
-        }
-        else if (question2 === "quavo" || question2 === "Quavo") {
+        if (question2 === "invisible" || question2 === "invisible") {
             return 2;
         }
-        else if (question2 === "offset" || question2 === "Offset") {
+        else if (question2 === "Fly" || question2 === "fly") {
             return 3;
         }
+
         else {
-            return 0;
+            return 1;
         }
     }
 
     function CalculateQuestion3Score(question3) {
         //console.log(question3);
-        if (question3 === "cat" || question3 === "Cat") {
+        if (question3 === "ceviche" || question3 === "Ceviche") {
             return 2;
         }
-        else if (question3 === "dog" || question3 === "Dog") {
+        else if (question3 === "Tacos" || question3 === "tacos") {
             return 3;
         }
         else {
             return 1;
         }
-    
+
     }
 
     function placement(totalScore,name) {
@@ -74,18 +68,18 @@ $(document).ready(function() {
         var imgSrc;
         console.log(totalScore);
         if (totalScore > 11) {
-            techplacement = "You Smart";
-            imgSrc = "https://i.makeagif.com/media/1-18-2017/AGhPEs.gif";
+            techplacement = "Superman";
+            imgSrc = "https://images-na.ssl-images-amazon.com/images/I/81%2Bx0iZ9MEL._AC_SX425_.jpg";
             displayResult(techplacement,name,imgSrc);
         }
         else if (totalScore > 8 && totalScore <= 11) {
-            techplacement = "The Office";
-            imgSrc = "https://i.pinimg.com/originals/62/99/ce/6299ce554436cd39b9862b2f6438f718.jpg";
+            techplacement = "Black Widow";
+            imgSrc = "2wCEAAkGBxMTEhUTExMWFhUXFxoYGBgYGBcYGBgXFxcWFx0aGBgYHSggGBolHRcXITEhJSkrLi4uFx8zODMtNygtLisBCgoKDg0OGxAQGy0lHyUtLS0vLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLf";
             displayResult(techplacement,name,imgSrc);
         }
         else if (totalScore == 5) {
-            techplacement = "Ug lee";
-            imgSrc = "https://i.pinimg.com/originals/62/99/ce/6299ce554436cd39b9862b2f6438f718.jpg";
+            techplacement = "batmannnn";
+            imgSrc = "https://images-na.ssl-images-amazon.com/images/I/810c4ywQsEL._AC_SY679_.jpg";
             displayResult(techplacement,name,imgSrc);
         }
         else {
